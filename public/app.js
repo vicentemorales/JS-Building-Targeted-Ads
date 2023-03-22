078bc7edabf80cdc0e3523a8032b3f4662de751d
vicentemorales <vmorales121@gmail.com>
2023-03-21 20:18:58 -0700

    personalized ads

--- public/app.js
+++ public/app.js
@@ -1,20 +1,69 @@
-// get user's data
-// get user's coordinates
+// get user's data                                                             
+// Get the user's coordinates:                                                              
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
 
 
-// get user's time
+             
 
 
+// Get the user's time:                                                              
+                                                              
+function userTime(){
+    const now = new Date()
+    return now.getHours()
+}
+                            
+                             
+
+// Get the user's time:                                                              
+function getMealTime(){
+    const tod = userTime()
+    
+    if (tod > 20) {return 'late-night snack'}
+    else if (tod > 16) {return 'dinner'}
+    else if (tod > 11) {return 'lunch'}
+    else {return 'breakfast'}
+    
+    // A ternary is another way to write an if-else statement
+    // Another way to write the above lines would to refactor it as:
+    // return tod > 20 ? 'late-night snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast' // <--- this is an example of a ternary
+}
+    
+                    
 // helper functions
 // check time of day
 
-
 // build ads
-// build ad 1
+// Build Ad 1:                                                           
+function buildAd1(){
+    const mealTime = getMealTime()
+    let content = document.querySelector('.ad1')
+    let inner = document.createElement('p')
+    inner.innerHTML = `We've got the best <span>${mealTime}</span> in town`
+    content.append(inner)
+}
+buildAd1()
 
 
-// build ad 2
-
+// Build Ad 2                                                             
+function buildAd2(coordinates){
+    const coords = coordinates
+    const href = `https://www.google.com/maps/search/sashimi/@${coords[0]},${coords[1]},15z/`
+    let content = document.querySelector('.ad2')
+    let inner = document.createElement('p')
+    inner.innerHTML = `It's time to try our coffee! <span><a href="${href}" target="_blank">We're this close!</a></span>`
+    content.append(inner)
+}
 
 // event listeners
-// on load, build ads
+// On load, build ads:                                                             
+window.onload = async () => {
+    const coords = await getCoords()
+    buildAd2(coords)
+}
+
